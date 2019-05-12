46568ccb073cbbcb6b0b316cabe7382f7fd7a613
Jasina <jjasinski@gog.com>
2019-05-12 18:14:24 +0200

    woking drawing features and added matcher

--- main.go
+++ main.go
@@ -5,26 +5,46 @@ import (
 	"image"
 	"image/color"
 	"os"
-	"strconv"
 
 	"gocv.io/x/gocv"
 )
 
+type Mapp struct {
+	Frames []gocv.Mat
+}
+
+func NewMap() Mapp {
+	return Mapp{}
+}
+
+func (m *Mapp) AddFrame(frame gocv.Mat) []gocv.Mat {
+	return append(m.Frames, frame)
+}
+
+func drawFeatures(img *gocv.Mat, features gocv.Mat, color color.RGBA) {
+	for i := 0; i < features.Rows(); i++ {
+		v := features.GetVecfAt(0, i)
+		// if circles are found
+		if len(v) > 1 {
+			x := int(v[0])
+			y := int(v[1])
+
+			gocv.Circle(img, image.Pt(x, y), 5, color, 2)
+		}
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("How to run:\n\tgo-slam [videofile] [line] [axis (x/y)] [with] ")
+		fmt.Println("How to run:\n\tgo-slam [videofile]")
 		return
 	}
 
 	filename := os.Args[1]
 
-	line, _ := strconv.Atoi(os.Args[2])
-	axis := os.Args[3]
-	width, _ := strconv.Atoi(os.Args[4])
-
 	video, err := gocv.VideoCaptureFile(filename)
 	if err != nil {
-		fmt.Println("Error reading video from: %v", filename)
+		fmt.Println("Error reading video from: ", filename)
 		return
 	}
 	defer video.Close()
@@ -35,57 +55,51 @@ func main() {
 	img := gocv.NewMat()
 	defer img.Close()
 
-	imgFG := gocv.NewMat()
-	defer imgFG.Close()
+	dest := gocv.NewMat()
+	defer dest.Close()
 
-	imgCleaned := gocv.NewMat()
-	defer imgCleaned.Close()
+	corners := gocv.NewMat()
+	defer corners.Close()
 
-	mog2 := gocv.NewBackgroundSubtractorMOG2()
-	defer mog2.Close()
+	mapp := NewMap()
+	blue := color.RGBA{0, 0, 255, 0}
+	// red := color.RGBA{255, 0, 0, 0}
 
-	count := 0
 	for {
 		if ok := video.Read(&img); !ok {
-			fmt.Println("Error reading video from: %v", filename)
+			fmt.Println("Error reading video from: ", filename)
 			return
 		}
 		if img.Empty() {
+			fmt.Println("Image from video: ", filename, "is empty")
 			continue
 		}
 
-		mog2.Apply(img, &imgFG)
-		kernel := gocv.GetStructuringElement(gocv.MorphRect, image.Pt(3, 3))
-		gocv.Erode(imgFG, &imgCleaned, kernel)
-		kernel.Close()
+		gocv.CvtColor(img, &dest, gocv.ColorBGRAToGray)
 
-		moments := gocv.Moments(imgCleaned, true)
-		area := moments["m00"]
+		gocv.GoodFeaturesToTrack(dest, &corners, 5000, 0.01, 7.0)
 
-		if area >= 1 {
-			x := int(moments["m10"] / area)
-			y := int(moments["m01"] / area)
+		if corners.Empty() {
+			fmt.Println("No corners found")
+		} else {
+			mapp.AddFrame(corners)
+			drawFeatures(&img, corners, blue)
+		}
 
-			if axis == "y" {
-				if x > 0 && x < img.Cols() && y > line && y < line+width {
-					count++
-				}
-				gocv.Line(&img, image.Pt(0, line), image.Pt(img.Cols(), line), color.RGBA{255, 0, 0, 0}, 2)
-			}
-			if axis == "x" {
-				if y > 0 && y < img.Rows() && x > line && x < line+width {
-					count++
-				}
-				gocv.Line(&img, image.Pt(line, 0), image.Pt(line, img.Rows()), color.RGBA{255, 0, 0, 0}, 2)
-			}
+		framesCount := len(mapp.Frames)
+		if framesCount > 1 {
+			bf := gocv.NewBFMatcher()
+			defer bf.Close()
 
-			gocv.PutText(&img, fmt.Sprintf("Count: %d ", count), image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, color.RGBA{0, 255, 0, 0}, 2)
+			bf.KnnMatch(mapp.Frames[framesCount-1], mapp.Frames[framesCount-2], 2)
 
-			window.IMShow(img)
-			if window.WaitKey(1) >= 0 {
-				break
-			}
+		}
+
+		gocv.PutText(&img, fmt.Sprintf("Count: %d ", corners.Total()), image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, color.RGBA{0, 255, 0, 0}, 2)
+
+		window.IMShow(img)
+		if window.WaitKey(1) >= 0 {
+			break
 		}
 	}
-
 }
